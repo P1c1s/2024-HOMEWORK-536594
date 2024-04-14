bdc793860aafbf1f771964c27e3cbc2432854f09
Lorenzo Ricciardi <56824204+P1c1s@users.noreply.github.com>
2024-04-14 20:37:24 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,32 @@
+package it.uniroma3.diadia.giocatore;
+
+public class Giocatore {
+	
+	static final private int CFU_INIZIALI = 20;
+	
+	private Borsa borsa;
+	private int cfu;
+	
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+	
+	public int getCfu() {
+		return this.cfu;
+	}
+	
+	public void setCfu(int cfu) {
+		if(cfu <= 20 && cfu >= 0)
+			this.cfu = cfu;		
+	}	
+
+	public void setBorsa(Borsa borsa){
+		this.borsa = borsa;
+	}
+	
+	public Borsa getBorsa(){
+		return this.borsa;
+	}
+	
+}
---
+++ test/it/uniroma3/diadia/giocatore/GiocatoreTest.java
@@ -0,0 +1,60 @@
+package it.uniroma3.diadia.giocatore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GiocatoreTest {
+
+	/* testSetCfu - INZIO */
+	
+	@Test
+	public void testSetCfu_ValoreCasuale() {
+		Giocatore giocatore = new Giocatore();
+		giocatore.setCfu(5);
+		assertEquals(5, giocatore.getCfu());
+	}
+	
+	@Test
+	public void testSetCfu_ValoreZero() {
+		Giocatore giocatore = new Giocatore();
+		giocatore.setCfu(0);
+		assertEquals(0, giocatore.getCfu());
+	}
+	
+	@Test
+	public void testSetCfu_ValoreNegativo() {
+		Giocatore giocatore = new Giocatore();
+		giocatore.setCfu(-2);
+		assertNotEquals(-2, giocatore.getCfu());
+	}
+	
+	@Test
+	public void testSetCfu_ValoreInEccesso() {
+		Giocatore giocatore = new Giocatore();
+		giocatore.setCfu(21);
+		assertNotEquals(21, giocatore.getCfu());
+	}
+	
+	/* testSetCfu - FINE */
+	
+	/* testGetCfu - INZIO */
+	
+	@Test
+	public void testGetCfu_CfuIniziali() {	
+		Giocatore giocatore = new Giocatore();
+		assertEquals(20, giocatore.getCfu());		
+	}
+	
+//	@Test
+//	public void testGetCfu_CfuIniziali() {	
+//		Giocatore giocatore = new Giocatore();
+//		assertEquals(20, giocatore.getCfu());		
+//	}
+
+	/* testGetCfu - FINE */
+	
+}
+
+
